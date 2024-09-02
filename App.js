8dc9659e65271c08ecdb13312a2890e840fc09bb
Alem <alemneh@hotmail.com>
2024-09-01 17:40:20 -0700

    Initializing project

---
+++ App.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react'
+import { View, ActivityIndicator, StyleSheet } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import Tabs from './src/components/Tabs'
+import { useGetWeather } from './src/hooks/useGetWeather'
+import ErrorItem from './src/components/ErrorItem'
+
+const App = () => {
+  const [loading, error, weather] = useGetWeather()
+
+  if (weather && weather.list && !loading) {
+    return (
+      <NavigationContainer>
+        <Tabs weather={weather} />
+      </NavigationContainer>
+    )
+  }
+
+  return (
+    <View style={styles.container}>
+      {error ? (
+        <ErrorItem />
+      ) : (
+        <ActivityIndicator size={'large'} color={'blue'} />
+      )}
+    </View>
+  )
+}
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    flex: 1
+  }
+})
+export default App
